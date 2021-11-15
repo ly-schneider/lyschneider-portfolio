6cdd7d0733bdc954a9660808816f1bbf7db4319d
ly-schneider <lyschneider@icloud.com>
2021-11-15 08:52:38 +0100

    Update Scroll Snapping

--- static/js/app.js
+++ static/js/app.js
@@ -1,43 +1,21 @@
-window.onload = function() {
-    var dob = new Date("08/16/2008");
+window.onload = function () {
+  var dob = new Date("08/16/2008");
 
-    var month_diff = Date.now() - dob.getTime();
+  var month_diff = Date.now() - dob.getTime();
 
-    var age_dt = new Date(month_diff);
+  var age_dt = new Date(month_diff);
 
-    var year = age_dt.getUTCFullYear();
+  var year = age_dt.getUTCFullYear();
 
-    var age = Math.abs(year - 1970);
+  var age = Math.abs(year - 1970);
 
-    document.getElementById("age").innerHTML = age + " years old,";
+  document.getElementById("age").innerHTML = age + " years old,";
 
-    if (age > 15) {
-        document.getElementById("work").innerHTML = "currently working.";
-    } else {
-        document.getElementById("work").innerHTML = "still at school.";
-    }
+  if (age > 15) {
+    document.getElementById("work").innerHTML = "currently working.";
+  } else {
+    document.getElementById("work").innerHTML = "still at school.";
+  }
 }
 
-gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray('.one').forEach((section, i) => {
-  
-  if(section.getAttribute('data-color') !== null) {
-    
-    var colorAttr = section.getAttribute('data-color')
-    
-    gsap.to(".line", {
-      backgroundColor: colorAttr === "dark" ? gsap.getProperty("html", "--blue") : gsap.getProperty("html", "--purple"),
-      immediateRender: false,
-      scrollTrigger: {
-        trigger: section,
-        scrub: true,
-        start:'top bottom',
-        end: '+=100%',
-        markers: true
-      }
-    });
-
-  }
-  
-});
