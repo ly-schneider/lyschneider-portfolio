d79313df5b5b8ce044bc418f80000522fc627118
ly-schneider <lyschneider@icloud.com>
2022-08-29 11:46:46 +0200

    Fixed navigation collapse

--- static/js/app.js
+++ static/js/app.js
@@ -65,11 +65,3 @@ var $grid = $('.grid').imagesLoaded( function() {
     percentPosition: true
   });
 });
-
-// Navbar Collapse
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
-const bsCollapse = new bootstrap.Collapse(menuToggle)
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => { bsCollapse.toggle() })
-})
